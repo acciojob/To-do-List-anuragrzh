37df8116caae594cf0f93e2e3045ddb06e1ffdf2
anuragsoni5643 <sonianurag506@gmail.com>
2026-05-14 13:06:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+let todobtn=document.getElementById("addTodoBtn")
+let todolist=document.getElementById("todoList")
+let text=document.getElementById("newTodoInput")
+todobtn.onclick = function () {
+    if (text.value !== "") {
 
+        let li = document.createElement("li");
+
+        li.innerHTML = text.value;
+
+        todolist.appendChild(li);
+
+        text.value = "";
+    }
+};
